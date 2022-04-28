ee162ef62de06cc808bdf2a33108e165528b09fd
Michael Verhulst <michael@terminallabs.com>
2022-04-27 20:51:09 -0500

    more network stuff

--- compilepython/runner.py
+++ compilepython/runner.py
@@ -11,13 +11,15 @@ import uuid
 import multiprocessing
 from pathlib import Path
 
+import requests
 import click
 
 from cli_passthrough import cli_passthrough
 
-from jobrunner.progressengine import updt, register_run, get_checkpointlines, pbar
+from jobrunner.progressengine import updt, register_run, get_checkpointlines, track_run, get_track_data
 from jobrunner.config import tmp_dirs
 from jobrunner.utils import remove, create_dirs, init_runner_env
+from jobrunner.core import app, simplejob
 
 VERSION = "0.1"
 PROJECT_NAME = "compilepython"
@@ -33,6 +35,10 @@ def kickoff_run(uuid_name):
     p1 =  multiprocessing.Process(target=run, args=(uuid_name, True,))
     p1.start()
 
+def kickoff_tracker(uuid_name, checkpointlines):
+    p1 =  multiprocessing.Process(target=track_run, args=(uuid_name, checkpointlines, False,))
+    p1.start()
+
 def run(uuid_name, capturelogs=False):
     if capturelogs:
         sys.stdout = open('.tmp/runners/runs/' + uuid_name + "/process.out", "w")
@@ -42,13 +48,25 @@ def run(uuid_name, capturelogs=False):
     cli_passthrough("bash examples/processes/compilepython/01.sh")
     cli_passthrough("bash examples/processes/compilepython/02.sh")
 
+@app.route('/track')
+def urltrack():
+    init_runner_env()
+    uuid_name, checkpointlines = pre_run()
+    kickoff_run(uuid_name)
+    kickoff_tracker(uuid_name, checkpointlines)
+    return {"uuid_name": uuid_name}
+
+@app.route('/status/<uuid_name>')
+def urlstatus(uuid_name):
+    return get_track_data(uuid_name)
+
 @click.group(context_settings=context_settings)
 @click.version_option(prog_name=PROJECT_NAME.capitalize(), version=VERSION)
 @click.pass_context
 def cli(ctx):
     pass
 
-@click.group(name="runners")
+@click.group(name="runner")
 def run_group():
     pass
 
@@ -58,12 +76,60 @@ def run_cmd():
     uuid_name, checkpointlines = pre_run()
     run(uuid_name)
 
+@run_group.command("track")
+def track_cmd():
+    init_runner_env()
+    uuid_name, checkpointlines = pre_run()
+    kickoff_run(uuid_name)
+    time.sleep(1)
+    track_run(uuid_name, checkpointlines, pbar=False)
+
 @run_group.command("pbar")
 def pbar_cmd():
     init_runner_env()
     uuid_name, checkpointlines = pre_run()
     kickoff_run(uuid_name)
     time.sleep(1)
-    pbar(uuid_name, checkpointlines)
+    track_run(uuid_name, checkpointlines, pbar=True)
 
+@click.group(name="server")
+def server_group():
+    pass
+
+@server_group.command("serve")
+def serve_cmd():
+    app.run(debug=True, port=8080)
+
+@click.group(name="client")
+def client_group():
+    pass
+
+@client_group.command("pbar")
+def client_pbar_cmd():
+    def __track():
+        r = requests.get('http://127.0.0.1:8080/track')
+        message = r.json()
+        uuid_name = message["uuid_name"]
+        return message
+
+
+    def __get_status():
+        r = requests.get('http://127.0.0.1:8080/status/' + uuid_name)
+        message = r.json()
+        runs = message["runs"]
+        count = message["count"]
+        return runs, count
+
+    message = __track()
+    uuid_name = message["uuid_name"]
+
+    runs, count = __get_status()
+    updt(runs, count)
+    while count <= runs - 1:
+        runs, count = __get_status()
+        updt(runs, count)
+        time.sleep(1)
+
+cli.add_command(server_group)
+cli.add_command(client_group)
 cli.add_command(run_group)
